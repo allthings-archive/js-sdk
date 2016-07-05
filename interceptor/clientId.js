35e7b2560eb9b0d645c6c9d4eeedc6ff349f5852
Moritz Schwörer <mr.mosch@gmail.com>
2016-07-05 15:52:02 +0200

    bring back the client id

--- interceptor/clientId.js
+++ interceptor/clientId.js
@@ -3,11 +3,23 @@ import interceptor from 'rest/interceptor'
 export default interceptor({
 
   request: function (request, config) {
-    try {
-      if (config.clientId) request.params = { client_id: config.clientId }
-      return request
-    } catch (e) {
-      console.error(e)
+    let params = request.params || {}
+
+    if (request.clientId) {
+      params.clientId = config.clientId
+      request.params = params
     }
+
+    if (request.clientID) {
+      params.clientID = config.clientId
+      request.params = params
+    }
+
+    if (request.client_id) {
+      params.client_id = config.clientId
+      request.params = params
+    }
+
+    return request
   }
 })
