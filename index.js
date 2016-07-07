bee67c0eb81ec3fc2521d3ede994730191c7b9fd
Moritz Schwörer <mosch@users.noreply.github.com>
2016-07-07 09:50:47 +0200

    added callback to sdk (#2)
    * added callback to sdk
    * 5.0.0

--- index.js
+++ index.js
@@ -13,22 +13,22 @@ import clientIdInterceptor from './interceptor/clientId'
 // Utils
 import accessTokenSession from './utils/accessTokenSession'
 
-const auth = ({ path, clientId, uuid }) => {
+const auth = (path, clientId, uuid, callback) => {
   return rest
     .wrap(defaultRequest, { mixin: { withCredentials: true } })
     .wrap(mime, { mime: 'application/json' })
-    .wrap(accessToken, { uuid, clientId })
+    .wrap(accessToken, { uuid, clientId, callback })
     .wrap(clientIdInterceptor, { clientId })
     .wrap(csrf, { path: path + 'csrf-token' })
     .wrap(pathPrefix, { prefix: path })
     .wrap(errorCode, { code: 400 })
 }
 
-const api = ({ path, clientId, uuid }) => {
+const api = (path, clientId, uuid, callback) => {
   return rest
     .wrap(defaultRequest)
     .wrap(mime, { mime: 'application/json' })
-    .wrap(accessToken, { uuid, clientId })
+    .wrap(accessToken, { uuid, clientId, callback })
     .wrap(clientIdInterceptor, { clientId })
     .wrap(pathPrefix, { prefix: path })
     .wrap(errorCode, { code: 400 })
